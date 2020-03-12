806a7145e76055b9f23ee2073dca5627bc3ddb3c
Eugene Goman <eugen.goman@gmail.com>
2020-03-12 12:54:14 +0300

    first commit

---
+++ AllAbout/AllAbout/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AllAbout
+//
+//  Created by UjiN on 3/12/20.
+//  Copyright © 2020 UjiN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
